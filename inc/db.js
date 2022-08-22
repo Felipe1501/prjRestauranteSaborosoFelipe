9bf77290a5f4681aebf7f4ce9f775b1c0c8e3136
Felipe1501 <felipebsantos1501@gmail.com>
2022-08-22 09:24:27 -0300

    aula 180 fix: connecting NodeJS with MySQL

---
+++ inc/db.js
@@ -0,0 +1,11 @@
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'db_saboroso',
+    password: '1501',
+    port: 3306
+});
+
+module.exports = connection;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
-var express = require('express');
-var router = express.Router();
+const conn = require('./../inc/db');
+const express = require('express');
+const Routes = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+Routes.get('/', function(req, res, next) {
+  conn.query("SELECT * FROM tb_users ORDER BY name", (err, results)=>{
+    if (err){
+      res.send(err);
+    }else{
+      res.send(results);
+    }
+  })
 });
 
-module.exports = router;
+module.exports = Routes;
