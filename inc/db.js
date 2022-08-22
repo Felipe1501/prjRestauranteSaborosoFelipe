6717d95379bf96d5568941fc162a698433f5b300
Felipe1501 <felipebsantos1501@gmail.com>
2022-08-22 11:55:05 -0300

    aula 183 fix: organizing HEADER with Includes

--- inc/db.js
+++ inc/db.js
@@ -1,11 +1,10 @@
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-    host: "localhost",
+    host: "127.0.0.1",
     user: "root",
     database: "db_saboroso",
     password: "",
-    port: 3306
 });
 
 module.exports = connection;
