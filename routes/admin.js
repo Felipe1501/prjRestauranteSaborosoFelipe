f0635b0ebc1f3758ba7b80a249d912753fd599ea
Felipe1501 <felipebsantos1501@gmail.com>
2022-08-23 11:53:20 -0300

    aula 194 add: Links Menu

--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var users =  require("./../inc/users");
+var admin = require("./../inc/admin")
 var router = express.Router();
 
 router.use(function(req, res, next){
@@ -13,6 +14,12 @@ router.use(function(req, res, next){
 });
 //redis = armazena informações, armazena em chave e em valor
 
+router.use(function(req, res, next){
+    req.menus = admin.getMenus();
+
+    next();
+});
+
 router.get("/logout", function(req, res, next){
    delete req.session.user;
 
@@ -21,26 +28,28 @@ router.get("/logout", function(req, res, next){
 router.get("/", function(req, res, next){
 
 
-        res.render("admin/index");
+        res.render("admin/index", {
+            menus: req.menus
+        });
  
 
 });
 
 router.get("/contacts", function(req, res, next){
     res.render("admin/contacts", {
-        
+        menus: req.menus
     });
 });
 
 router.get("/emails", function(req, res, next){
     res.render("admin/emails", {
-        
+        menus: req.menus
     });
 });
 
 router.get("/menus", function(req, res, next){
     res.render("admin/menus", {
-        
+        menus: req.menus
     });
 });
 
@@ -70,13 +79,14 @@ router.get("/login", function(req, res, next){
 
 router.get("/reservations", function(req, res, next){
     res.render("admin/reservations", {
-        date: {}
+        date: {},
+        menus: req.menus
     });
 });
 
 router.get("/users", function(req, res, next){
     res.render("admin/users", {
-        
+        menus: req.menus
     });
 });
 
