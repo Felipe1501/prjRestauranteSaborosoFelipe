39952a98ae00b01c4893fcc57faf450c5ddd1c41
Felipe1501 <felipebsantos1501@gmail.com>
2022-08-25 10:52:47 -0300

    aula 217 fix: user administration changing password

--- public/js/felipe-grid.js
+++ public/js/felipe-grid.js
@@ -26,13 +26,15 @@ class FelipeGrid{
         this.options = Object.assign({}, {
         formCreate: '#modal-create form',
         formUpdate: '#modal-update form',
-        btnUpdate: '.btn-update',
-        btnDelete: '.btn-delete',
+        btnUpdate: 'btn-update',
+        btnDelete: 'btn-delete',
         onUpdateLoad: (form, name, data) =>{
             let input = form.querySelector('[name='+ name + ']');
            if(input) input.value = data[name];
         }
         }, configs);
+
+        this.rows = [...document.querySelectorAll('table tbody tr')];
 
         this.initForms();
         this.initButtons();
@@ -41,28 +43,25 @@ class FelipeGrid{
     initForms(){
       this.formCreate = document.querySelector(this.options.formCreate);
       
-      this.formCreate.save().then(json=>{
-
-        this.fireEvent('afterFormCreate');
-
-      }).catch(err =>{
-
-        this.fireEvent('afterFormCreateError');
-
-
+      this.formCreate.save({
+        sucess:()=>{
+          this.fireEvent('afterFormCreate');
+        },
+        failure:()=>{
+          this.fireEvent('afterFormCreateError');
+        }
       });
 
 
       this.formUpdate = document.querySelector(this.options.formUpdate);
 
-      this.formUpdate.save().then(json=>{
-
-        this.fireEvent('afterFormUpdate');
-
-      }).catch(err =>{
-
-        this.fireEvent('afterFormUpdateError');
-        
+      this.formUpdate.save({
+        sucess:()=>{
+          this.fireEvent('afterFormUpdate');
+        },
+        failure:()=>{
+          this.fireEvent('afterFormUpdateError');
+        }
       });
     }
 
@@ -80,34 +79,24 @@ class FelipeGrid{
       return JSON.parse(tr.dataset.row);
     }
 
-    initButtons(){
-        
+    btnUpdateClick(e){
+      this.fireEvent('beforeUpdateClick', [e]);
 
-      [...document.querySelectorAll(this.options.btnUpdate)].forEach(btn =>{
-        btn.addEventListener('click', e =>{
+      let data = this.getTrData(e);
 
-          this.fireEvent('beforeUpdateClick', [e]);
+      for (let name in data){
 
-          let data = this.getTrData(e);
-
-          for (let name in data){
-
-            this.options.onUpdateLoad(this.formUpdate, name, data);
+        this.options.onUpdateLoad(this.formUpdate, name, data);
 
 
-          }
+      }
 
 
-          this.fireEvent('afterUpdateClick', [e]);
+      this.fireEvent('afterUpdateClick', [e]);
+    }
 
-        });
-
-      });
-
-      [...document.querySelectorAll(this.options.btnDelete)].forEach(btn=>{
-        btn.addEventListener('click', e=>{
-
-          this.fireEvent('beforeDeleteClick');
+    btnDeleteClick(e){
+      this.fireEvent('beforeDeleteClick');
 
           let data = this.getTrData(e);
 
@@ -123,9 +112,26 @@ class FelipeGrid{
           });
           
         }
+    }
 
+    initButtons(){
+      
+      this.rows.forEach(row =>{
+        [...row.querySelectorAll('.btn')].forEach(btn =>{
+          btn.addEventListener('click', e =>{
+              if(e.target.classList.contains(this.options.btnUpdate)){
+                this.btnUpdateClick(e);
+              }else if(e.target.classList.contains(this.options.btnDelete)){
+                this.btnDeleteClick(e);
+              }else{
+                this.fireEvent('buttonClick', [e.target, this.getTrData(e), e]);
+
+              }
+          });
         });
       });
 
+
+    
     }
 }
--- public/js/formSave.js
+++ public/js/formSave.js
@@ -1,26 +1,27 @@
-HTMLFormElement.prototype.save = function(){
+HTMLFormElement.prototype.save = function(configs){
 
-    let form = this
+    let form = this;
+
+    form.addEventListener('submit', e =>{
+      e.preventDefault();
+
+      let formData = new FormData(form); //obj nativo para gerenciar dados de um forms
     
-    return new Promise((resolve, reject)=>{
-        form.addEventListener('submit', e =>{
-            e.preventDefault();
-      
-            let formData = new FormData(form); //obj nativo para gerenciar dados de um forms
-          
-            fetch(form.action, {
-              method: form.method,
-              body: formData
-            })
-            .then(response => response.json())
-            .then(json=>{
-      
-              resolve(json);
-      
-            }).catch(err =>{
-                reject(err);
-            });
-          });
+      fetch(form.action, {
+        method: form.method,
+        body: formData
+      })
+      .then(response => response.json())
+      .then(json=>{
+        if(json.error){
+          if (typeof configs.failure === 'function') configs.failure(json.error);
+        }else{
+          if (typeof configs.sucess === 'function') configs.sucess(json);
+        }
+
+      }).catch(err =>{
+        if (typeof configs.failure === 'function') configs.failure(err);
+      });
     });
 
 }
